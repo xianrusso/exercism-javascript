264787eb07c75786c75af5162071675b325dc869
xian <iamchristianrusso@gmail.com>
2019-04-22 17:52:17 -0400

    modify word-count

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,4 +1,17 @@
+// Updated Solution using hasOwnProperty
 export class Words {
+    count(str) {
+        let words = {};
+        str.toLowerCase().trim().split(/\s+/).map(i => {
+            words[i] = (words.hasOwnProperty(i) ? words[i] += 1 : 1);
+        })
+        return words;
+    }
+}
+
+
+// Original Solution (add 's' to 'Word')
+class Word {
     count(str) {
         let arr = str.toLowerCase().trim().split(/\s+/);
         let words = Object.create(null);
